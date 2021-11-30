46b1009ad95fabe623ed3a2eabbfce75f28a708b
nakajima20210516 <c200029@ccg.ac.jp>
2021-11-30 07:00:04 +0000

    1

--- list_u.php
+++ list_u.php
@@ -55,8 +55,7 @@ try{
 <p>データ件数：<?php echo $count;?>件</p>
 
 <div class="container">
-/*  (1)ここに適切な formタグを追加してください  */
-
+<form action =  "edit.php" method = "post">
 <table border=1>
     <tr><th>id</th><th>名前</th><th>メールアドレス</th><th>性別</th><th>選択対象</th></tr>
     <?php foreach($data as $row): ?>
@@ -76,8 +75,7 @@ try{
         ?>
     </td>
     <td>
-        /*  (2)ここにラジオボタン表示処理を追加して下さい  */
-
+      <input type="radio" name == >編集
     </td>
     </tr>
     <?php endforeach; ?>
